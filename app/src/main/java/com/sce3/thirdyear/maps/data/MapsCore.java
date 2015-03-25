7032ee125ab951ab80032b49acc17c207c851a6d
David Eini <david.eini.se@gmail.com>
2015-03-25 20:02:11 +0200

    Add maps core

---
+++ app/src/main/java/com/sce3/thirdyear/maps/data/MapsCore.java
@@ -0,0 +1,14 @@
+package com.sce3.thirdyear.maps.data;
+
+import com.google.android.gms.maps.GoogleMapOptions;
+
+/**
+ * Created by David on 23/03/2015.
+ */
+public class MapsCore {
+
+    private void initilize() {
+        GoogleMapOptions options;
+
+    }
+}
